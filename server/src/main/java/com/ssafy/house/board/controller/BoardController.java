0744b61465bbb83ab079a30b4d1ebc6001efad95
LeeHyoJin <sunjin980@gmail.com>
2023-05-24 02:28:35 +0900

    게시글 전체정보 받아오기 api추가

--- server/src/main/java/com/ssafy/house/board/controller/BoardController.java
+++ server/src/main/java/com/ssafy/house/board/controller/BoardController.java
@@ -1,6 +1,8 @@
 package com.ssafy.house.board.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.house.board.model.BoardDto;
 import com.ssafy.house.board.model.BoardParameterDto;
 import com.ssafy.house.board.service.BoardService;
+import com.ssafy.util.PageNavigation;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -38,7 +41,7 @@ public class BoardController {
 	@Autowired
 	private BoardService boardService;
 
-	@ApiOperation(value = "글!!!!!!!!!작!!!!!!!!!성!!!!!!!!", notes = "새로운 게시글 정보를 입력한다. 그리고 DB입력 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+	@ApiOperation(value = "게시판 글작성", notes = "새로운 게시글 정보를 입력한다. 그리고 DB입력 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
 	@PostMapping
 	public ResponseEntity<String> writeArticle(@RequestBody @ApiParam(value = "게시글 정보.", required = true) BoardDto boardDto) throws Exception {
 		logger.info("writeArticle - 호출");
@@ -48,14 +51,14 @@ public class BoardController {
 		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
 	}
 	
-	@ApiOperation(value = "게시판 글목록!!!!!!!!", notes = "모든 게시글의 정보를 반환한다.", response = List.class)
+	@ApiOperation(value = "게시판 글목록", notes = "모든 게시글의 정보를 반환한다.", response = List.class)
 	@GetMapping
 	public ResponseEntity<List<BoardDto>> listArticle(@ApiParam(value = "게시글을 얻기위한 부가정보.", required = true) BoardParameterDto boardParameterDto) throws Exception {
 		logger.info("listArticle - 호출");
 		return new ResponseEntity<List<BoardDto>>(boardService.listArticle(boardParameterDto), HttpStatus.OK);
 	}
 	
-	@ApiOperation(value = "게시판 글보기!!!!!!!!!!!", notes = "글번호에 해당하는 게시글의 정보를 반환한다.", response = BoardDto.class)
+	@ApiOperation(value = "게시판 글보기", notes = "글번호에 해당하는 게시글의 정보를 반환한다.", response = BoardDto.class)
 	@GetMapping("/{articleno}")
 	public ResponseEntity<BoardDto> getArticle(@PathVariable("articleno") @ApiParam(value = "얻어올 글의 글번호.", required = true) int articleno) throws Exception {
 		logger.info("getArticle - 호출 : " + articleno);
@@ -63,7 +66,7 @@ public class BoardController {
 		return new ResponseEntity<BoardDto>(boardService.getArticle(articleno), HttpStatus.OK);
 	}
 	
-	@ApiOperation(value = "게시판 글수정!!!!!!!!!!!", notes = "수정할 게시글 정보를 입력한다. 그리고 DB수정 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+	@ApiOperation(value = "게시판 글수정", notes = "수정할 게시글 정보를 입력한다. 그리고 DB수정 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
 	@PutMapping
 	public ResponseEntity<String> modifyArticle(@RequestBody @ApiParam(value = "수정할 글정보.", required = true) BoardDto boardDto) throws Exception {
 		logger.info("modifyArticle - 호출 {}", boardDto);
@@ -74,7 +77,7 @@ public class BoardController {
 		return new ResponseEntity<String>(FAIL, HttpStatus.OK);
 	}
 	
-	@ApiOperation(value = "게시판 글삭제!!!!!!!", notes = "글번호에 해당하는 게시글의 정보를 삭제한다. 그리고 DB삭제 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+	@ApiOperation(value = "게시판 글삭제", notes = "글번호에 해당하는 게시글의 정보를 삭제한다. 그리고 DB삭제 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
 	@DeleteMapping("/{articleno}")
 	public ResponseEntity<String> deleteArticle(@PathVariable("articleno") @ApiParam(value = "살제할 글의 글번호.", required = true) int articleno) throws Exception {
 		logger.info("deleteArticle - 호출");
@@ -82,5 +85,22 @@ public class BoardController {
 			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
 		}
 		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+	}
+	
+	
+	@ApiOperation(value = "게시판 정보 받기", notes = "총 게시글 수", response = String.class)
+	@GetMapping("/count")
+	public ResponseEntity<?> count(BoardParameterDto boardParameterDto) throws Exception {
+		try {
+			List<BoardDto> list = boardService.listArticle(boardParameterDto);
+			PageNavigation pageNavigation = boardService.makePageNavigation(boardParameterDto);
+			Map<String, Object> map = new HashMap<>();
+			map.put("list", list);
+			map.put("pagenavigation", pageNavigation);
+			return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>("서버 오류", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
