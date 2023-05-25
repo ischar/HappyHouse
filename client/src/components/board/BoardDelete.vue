d1bcb82bffe9231e2fb4e95a54abdb1a6dd02380
hjin2 <sunjin980@gmail.com>
2023-05-25 11:55:57 +0900

    삭제 디자인 추가

--- client/src/components/board/BoardDelete.vue
+++ client/src/components/board/BoardDelete.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="regist">
-    <h1>SSAFY 글 삭제</h1>
-    <div>삭제중....</div>
+    <img
+      src="../../assets/delete.png"
+      style="width: 300px; height: 300px opacity:80%;" />
+    <h3 style="margin-left: 50px">삭제중..</h3>
   </div>
 </template>
 
@@ -14,17 +16,25 @@ export default {
     console.log(this.$route.params.articleno);
     // 비동기
     // TODO : 글번호에 해당하는 글을 삭제.
-    http.delete(`/board/${this.$route.params.articleno}`).then(({ data }) => {
+    http.delete(`/board/${this.$route.params.articleno}`).then(({data}) => {
       let msg = "삭제 처리시 문제가 발생했습니다.";
       if (data === "success") {
         msg = "삭제가 완료되었습니다.";
       }
       alert(msg);
       // 현재 route를 /list로 변경.
-      this.$router.push({ name: "boardlist" });
+      this.$router.push({name: "boardlist"});
     });
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+.regist {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
